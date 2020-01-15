9cc16d6d8b7893e63dc723790388f29b2961e0ac
[Justin Schouten] <[302807329@student.rocmondriaan.nl]>
2020-01-15 15:07:44 +0100

    8.3

---
+++ Oracle/8.3/ExceptionsEx/src/com/example/Testen.java
@@ -0,0 +1,20 @@
+package com.example;
+public class Testen {
+
+    public static void main(String[] args) {
+        int d = 0;
+        int a = 10 / d;
+        System.out.println(a);
+
+
+        int divisor = 0;
+        if (divisor == 0)
+        {
+            System.out.println("Can't be zero!");
+        }
+        else
+        {
+            System.out.println(5 / divisor);
+        }
+    }
+}
---
+++ Oracle/8.3/ExceptionsEx/src/com/example/Testen2.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import java.io.IOException;
+
+//public class File {
+////    public static void main(String[] args) {
+////        try
+////        {
+////            File testFile = new File("//testFile.txt");
+////            testFile.createNewFile();
+////            System.out.println("testFile exists:"
+////            + testFile.exists());
+////        }
+////        catch (IOException e)
+////        {
+////            System.out.println(e);
+////        }
+////    }
+//}
