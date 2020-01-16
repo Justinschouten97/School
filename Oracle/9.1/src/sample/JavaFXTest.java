89c338b8984bf02663e9867339672e9d23ae2af7
[Justin Schouten] <[302807329@student.rocmondriaan.nl]>
2020-01-16 12:46:40 +0100

    JavaFxTest toegevoegd met veel functies

--- Oracle/9.1/src/sample/JavaFXTest.java
+++ Oracle/9.1/src/sample/JavaFXTest.java
@@ -1,46 +1,148 @@
+/*
+ @author Justin Schouten
+ (Veel functies toegevoegd)
+ */
+
 package sample;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import javafx.scene.control.TextField;
 
-public class JavaFXTest extends Application
-{
-    Button bt1;
-    Button btn2;
 
-    public static void main(String[] args)
-    {
+import javafx.scene.shape.Rectangle;
+
+public class JavaFXTest extends Application {
+
+        @Override
+        public void start (Stage primaryStage)
+        {
+            Button btn1 = new Button();
+            btn1.setText("Klik hier 1");
+            btn1.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    System.out.println("Je hebt geklikt op button 1");
+                }
+            });
+
+                Button btn2 = new Button();
+                btn2.setText("Klik hier 2");
+            btn2.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        System.out.println("Je hebt geklikt op button 2");
+                    }
+                });
+            Button btn3 = new Button();
+            btn3.setText("Klik hier 3");
+            btn3.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    System.out.println("Je hebt geklikt op button 3");
+                }
+            });
+            Button btn4 = new Button();
+            btn4.setText("Klik hier 4");
+            btn4.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    System.out.println("Je hebt geklikt op button 4");
+                }
+            });
+
+            TextField t1 = new TextField();
+
+            Rectangle rt1 = new Rectangle();
+
+            rt1.setWidth(50.0d);
+            rt1.setHeight(50.0d);
+//            rt1.setX(1);
+//            rt1.setY(1);
+
+
+            ScrollBar sb1 = new ScrollBar();
+            ScrollBar sb2 = new ScrollBar();
+
+            PieChart pc1 = new PieChart();
+
+            TextField text = new TextField("Text");
+            text.setMaxSize(140, 20);
+
+            ChoiceBox cb = new ChoiceBox();
+            cb.getItems().addAll("Ja", "Nee", "Misschien");
+            cb.getSelectionModel().selectFirst();
+
+            ToolBar toolbar = new ToolBar();
+            toolbar.getItems().add(new Button("Home"));
+            toolbar.getItems().add(new Button("Options"));
+            toolbar.getItems().add(new Button("Help"));
+
+
+//            BorderPane borderPane = new BorderPane();
+
+            // Group 1
+            Group po = new Group();
+
+            // Bodem Content //
+            HBox root = new HBox();
+            root.getChildren().add(btn3);
+            root.getChildren().add(btn4);
+            //plaatst de Hbox onderaan het scherm
+            root.setLayoutY(210);
+
+
+
+            //HBox in de Groep (po) plaatsen
+            po.getChildren().add(root);
+
+
+            // Top Content //
+            VBox topVbox = new VBox();
+            topVbox.getChildren().add(toolbar);
+            topVbox.setLayoutX(0);
+            topVbox.setLayoutY(45);
+            //Toolbar in de Groep (po) plaatsen
+            po.getChildren().add(toolbar);
+
+            // Left Content //
+            VBox leftVBox = new VBox();
+            leftVBox.setLayoutY(50);
+            leftVBox.getChildren().add(btn1);
+            leftVBox.getChildren().add(btn2);
+            leftVBox.getChildren().add(t1);
+//            leftVBox.getChildren().add(rt1); // rectangle geeft hij niet weer
+            leftVBox.getChildren().add(text);
+            leftVBox.getChildren().add(cb);
+            leftVBox.getChildren().add(sb1);
+//            leftVBox.getChildren().add(pc1);  // piechart geeft hij niet weer
+            root.getChildren().add(leftVBox);
+
+            //VBox in de Groep (po) plaatsen
+            po.getChildren().add(leftVBox);
+
+
+
+        Scene scene = new Scene(po, 300, 250);
+
+        primaryStage.setTitle("Hello Buttons!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
         launch(args);
     }
-
-
-    @Override
-    public void start(Stage primaryStage) throws Exception
-    {
-        primaryStage.setTitle("Buttons Testen");
-        Button btn1 = new Button();
-        btn1.setText("Button 1");
-
-        Button btn2 = new Button();
-        btn2.setText("Button 2");
-
-        // THis class will handle the button events
-        btn1.setOnAction(e -> System.out.println("Je hebt geklikt op button 1"));
-        btn2.setOnAction(e -> System.out.println("Je hebt geklikt op button 2"));
-
-
-        StackPane layout = new StackPane();
-        layout.getChildren().add(btn1);
-        Scene scene1 = new Scene(layout, 300, 250);
-        primaryStage.setScene(scene1);
-        primaryStage.show();
-    }
 }
-
-
--- Oracle/9.1/src/sample/JavaFXTest2.java
+++ Oracle/9.1/src/sample/JavaFXTest2.java
@@ -4,48 +4,43 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
 public class JavaFXTest2 extends Application
 {
-    Stage window;
-    Scene scene1, scene2;
+    Button bt1;
+    Button btn2;
 
     public static void main(String[] args)
     {
         launch(args);
     }
 
+
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        window = primaryStage;
+        primaryStage.setTitle("Buttons Testen");
+        Button btn1 = new Button();
+        btn1.setText("Button 1");
 
-        Label label1 = new Label("Welcome to the first scene!");
-        Button button1 = new Button("Go to scene 2");
-        button1.setOnAction(e -> window.setScene(scene2));
+        Button btn2 = new Button();
+        btn2.setText("Button 2");
 
-        // Layout 1 = childeren are laid out in vertical column
-        VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label1, button1);
-        scene1 = new Scene(layout1, 200, 200);
+        // THis class will handle the button events
+        btn1.setOnAction(e -> System.out.println("Je hebt geklikt op button 1"));
+        btn2.setOnAction(e -> System.out.println("Je hebt geklikt op button 2"));
 
-        // Button 2
-        Button button2 = new Button("Go to scene 1");
-        button2.setOnAction(e -> window.setScene(scene1));
 
-        // Layout 2
-        StackPane layout2 = new StackPane();
-        layout2.getChildren().add(button2);
-        scene2 = new Scene(layout2,600,300);
-
-        window.setScene(scene1);
-        window.setTitle("Switch page button");
-        window.show();
+        StackPane layout = new StackPane();
+        layout.getChildren().add(btn1);
+        Scene scene1 = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene1);
+        primaryStage.show();
     }
 }
+
+
