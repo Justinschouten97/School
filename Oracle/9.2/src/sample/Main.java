bda59ec3a3e87ed8b5bda441118ba9b05b7a87dc
justin <302807392@student.rocmondriaan.nl>
2020-01-16 17:07:35 +0100

    9.2 af

--- Oracle/9.2/src/sample/Main.java
+++ Oracle/9.2/src/sample/Main.java
@@ -1,42 +1,53 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
-import javafx.scene.control.*;
-
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 
+import java.awt.*;
 
 
 public class Main extends Application
 {
-
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        // Rectagles //
-        // Rectangle 1 == rect
-        Rectangle rect = new Rectangle(20,20,100,200);
-        rect.setFill(Color.GREEN);
-        rect.setStroke(Color.BLACK);
-        rect.setStrokeWidth(5.0);
+        // Polygon //
+        Polygon polygon1 = new Polygon(new double[]{
+                80 , 10 ,
+                10 , 80 ,
+                80 , 80 ,
+        });
+//        Stop[] stops = new Stop[] { new Stop(0, Color.BLACK), new Stop(1, Color.BLUE)};
+//        LinearGradient lg1 = new LinearGradient(0, 0, 1, 0, true,
+//                CycleMethod.NO_CYCLE, stops);
 
+        LinearGradient gradient1 = new LinearGradient(0,0,1,0,true,
+                CycleMethod.NO_CYCLE, new Stop[] {
+            new Stop(1, Color.DODGERBLUE),
+            new Stop(0, Color.BLACK)
 
-
+        });
 
         // Groepen //
         // Groep 1
         Group g1 = new Group();
 
         // Objecten in de groep (om de positie aan te passen (Niet in een HBox plaatsen!))
-        g1.getChildren().add(rect);
+//        g1.getChildren().add(rect);
+        g1.getChildren().add(polygon1);
+        g1.setLayoutX(20);
+        g1.setLayoutY(20);
+
+//        polygon1.setFill(Color.web("#0f65ee"));
+        polygon1.setFill(gradient1);
 
         // HBoxen //
         // HBox 1
@@ -49,7 +60,7 @@ public class Main extends Application
 
         // Eerste pagina
 //        Scene scene1 = new Scene(g1, 300, 275, Color.rgb(255,0,0,0.2));
-        Scene scene1 = new Scene(g1, 300, 275, Color.rgb(0,0,255,0.8));
+        Scene scene1 = new Scene(g1, 300, 275, Color.rgb(0,0,0,1.0));
 
 
         primaryStage.setTitle("Hello World");
