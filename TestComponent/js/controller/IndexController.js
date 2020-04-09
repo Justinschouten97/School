d1d628da0b3f6099a9c5f7546315ac28e4b2db44
Red343 <56629055+Justinschouten97@users.noreply.github.com>
2020-04-09 18:25:05 +0200

    Add files via upload

---
+++ TestComponent/js/controller/IndexController.js
@@ -0,0 +1,37 @@
+window.addEventListener("load", (e) => {
+    let index = new IndexController();
+    index.init();
+});
+
+class IndexController {
+
+    init() {
+        this.correctUsername = "admin";
+        this.correctPassword = "admin";
+
+        let formEl = document.getElementById('loginForm');
+        // Listen to the submit event
+        formEl.addEventListener("submit", this.onSubmit)
+    }
+
+    onSubmit(event) {
+        event.preventDefault(); // prevent the default request
+        // Get the values from the form
+        let username = document.getElementById("name").value;
+        let password = document.getElementById("password").value;
+
+        // Check whether the correct username and password are the same as the form values
+        if (username === this.correctUsername && password === this.correctPassword) {
+            document.getElementById("login").textContent = "Login succes!";
+        } else {
+            // Get all the input elements with the <input> tag
+            let inputs = document.getElementsByTagName("input");
+            for (const input of inputs) {
+                // Add the class error which colors the borders red
+                input.classList.add('error');
+            }
+            // Used textContent to prevent XSS
+            document.getElementById("errorText").textContent = "Login mislukt!";
+        }
+    }
+}
