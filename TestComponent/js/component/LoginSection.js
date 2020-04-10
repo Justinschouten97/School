6ccdfbe677639e6da4704bbbc6f3df017890c830
justin <302807392@student.rocmondriaan.nl>
2020-04-10 19:40:21 +0200

    FInised Login component

--- TestComponent/js/component/LoginSection.js
+++ TestComponent/js/component/LoginSection.js
@@ -16,5 +16,31 @@ class LoginSection extends Component {
                 <button class="button">Login</button>
             </form>
             <p id="errorText"></p> `;
+        let formEl = this.getElementbyId("loginForm");
+        // Listen to the submit event // is een door geef luik geworden (event) => {this...(event); });
+        formEl.addEventListener("submit", (event) => { // <-- Arrow function so this stay this
+            this.onSubmit(event);
+        });
+    }
+
+    onSubmit(event) {
+        event.preventDefault(); // prevent the default request
+        // Get the values from the form
+        let username = this.getElementbyId("name").value;
+        let password = this.getElementbyId("password").value;
+
+        // Check whether the correct username and password are the same as the form values
+        if (username === this.correctUsername && password === this.correctPassword) {
+            this.getElementbyId("login").textContent = "Login succes!";
+        } else {
+            // Get all the input elements with the <input> tag
+            let inputs = this.rootElement.getElementsByTagName("input");
+            for (const input of inputs) {
+                // Add the class error which colors the borders red
+                input.classList.add('error');
+            }
+            // Used textContent to prevent XSS
+            this.getElementbyId("errorText").textContent = "Login mislukt!";
+        }
     }
 }
