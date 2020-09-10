c743967b75193603b6e1bf076a3876fc199571dd
justin <302807392@student.rocmondriaan.nl>
2020-09-11 00:04:26 +0200

    H4 Opdracht 5 af

---
+++ Leerjaar2_2ha/Sprint1/PHPdeel1/hoofdstuk_4_opdr_5.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Opdracht 4 H4</title>
+    </head>
+    <body>
+        <?php
+
+        $getal = rand(0,20);
+        echo "Is het getal $getal even? ";
+
+        if ($getal % 2 == 0){
+            echo "Ja";
+        }
+        else{
+            echo "Nee";
+        }
+
+        ?>
+    </body>
+</html>
