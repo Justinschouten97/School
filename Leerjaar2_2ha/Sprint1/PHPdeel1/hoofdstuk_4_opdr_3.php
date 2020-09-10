1d933e5f0fd6382e7ff739c44276142635adce19
justin <302807392@student.rocmondriaan.nl>
2020-09-10 22:51:28 +0200

    H4 opdracht 3

---
+++ Leerjaar2_2ha/Sprint1/PHPdeel1/hoofdstuk_4_opdr_3.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <?php
+
+        // invoer van tijdzone
+        date_default_timezone_set("Europe/Amsterdam");
+
+        // invoer van de tijd (uur + min)
+        $date = date("H:i");
+        echo "$date";
+
+        echo "<br>";
+
+        // invoer van uur om te gebruiken in de switch
+        $uur = date("H");
+
+        switch ($uur){
+
+            case ("$uur" >= 6) && ("$uur" < 12) : echo "Het is Ochtend";
+                break;
+            case ("$uur" >= 12) && ("$uur" < 18): echo "Het is Middag";
+                break;
+            case ("$uur" > 18) && ("$uur" < 24): echo "Het is Avond";
+                break;
+
+            default: echo "Het is Nacht";
+        }
+
+        ?>
+    </body>
+</html>
