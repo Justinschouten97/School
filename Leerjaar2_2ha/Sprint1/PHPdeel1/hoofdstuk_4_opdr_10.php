648bf812b7aac64f5e281d2ae5a096ce2d2624a9
justinschouten97 <302807392@student.rocmondriaan.nl>
2020-09-21 12:11:06 +0200

    vast bij opdr 10 en 11

---
+++ Leerjaar2_2ha/Sprint1/PHPdeel1/hoofdstuk_4_opdr_10.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Opdracht 10 H4</title>
+    </head>
+    <body>
+        <?php
+
+        $getal = 4;
+        $som = 0;
+
+
+        for ($i = 0; $i <= $getal; $i++){
+
+            $som =- $i;
+
+            echo $getal + $som;
+            echo " + ";
+
+            $som++;
+        }
+
+
+        echo " = $getal";
+
+        ?>
+    </body>
+</html>
