b1bed2cfd9356e6b8ae790ea9cc1aa47e7ff285b
justin <302807392@student.rocmondriaan.nl>
2020-09-30 17:02:57 +0200

    H4 opdr 13

--- Leerjaar2_2ha/Sprint1/PHPdeel1/hoofdstuk_4_opdr_13.php
+++ Leerjaar2_2ha/Sprint1/PHPdeel1/hoofdstuk_4_opdr_13.php
@@ -5,7 +5,29 @@
     <body>
         <?php
 
+        echo "<table border='1'>";
 
+        $miles = 1;
+        $kilometer = 1609; // 1 Kilometer is 1609 Miles
+
+        // headers van de table
+        echo "<th>Miles</th>";
+        echo "<th>Kilometers</th>";
+
+        for ($i = 1; $i < 11; $i++){
+
+            // variabele
+            $nieuwKilometer = $kilometer * $miles;
+
+            // tabel
+            echo "<tr>";
+            echo "<td>$i</td>";
+            echo "<td>$nieuwKilometer</td>";
+            echo "</tr>";
+
+            $miles++;
+        }
+        echo "</table>";
 
         ?>
     </body>
