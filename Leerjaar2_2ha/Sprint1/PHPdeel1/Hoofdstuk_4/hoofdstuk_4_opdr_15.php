4e437d0aa0163c3d9fbe8afbb9757ad88ab734b0
justin <302807392@student.rocmondriaan.nl>
2020-10-01 04:00:44 +0200

    nog bezig met H4 opdr 15

---
+++ Leerjaar2_2ha/Sprint1/PHPdeel1/Hoofdstuk_4/hoofdstuk_4_opdr_15.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+    <title>Opdracht 15 H4</title>
+</head>
+<body>
+    <?php
+    echo "<table border='1'>";
+
+    // I
+    echo "<th>I</th>";
+    echo "<tr>";
+    echo "<td>";
+    echo "<br>";
+    for ($i = 1; $i <= 6; $i++) {echo $i . " ";}
+    echo "<br>";
+    for ($i = 1; $i <= 5; $i++) {echo $i . " ";}
+    echo "<br>";
+    for ($i = 1; $i <= 4; $i++) {echo $i . " ";}
+    echo "<br>";
+    for ($i = 1; $i <= 3; $i++) {echo $i . " ";}
+    echo "<br>";
+    for ($i = 1; $i <= 2; $i++) {echo $i . " ";}
+    echo "<br>";
+    for ($i = 1; $i <= 1; $i++) {echo $i . " ";}
+    echo "<br>";
+    echo "</td>";
+    echo "</tr>";
+
+    echo "<br>";
+
+    // II
+    echo "<th>II</th>";
+    echo "<tr>";
+    echo "<td>";
+    echo "<br>";
+    for ($i = 1; $i <= 1; $i++) {echo $i . " ";}
+    echo "<br>";
+    for ($i = 1; $i <= 2; $i++) {echo $i . " ";}
+    echo "<br>";
+    for ($i = 1; $i <= 3; $i++) {echo $i . " ";}
+    echo "<br>";
+    for ($i = 1; $i <= 4; $i++) {echo $i . " ";}
+    echo "<br>";
+    for ($i = 1; $i <= 5; $i++) {echo $i . " ";}
+    echo "<br>";
+    for ($i = 1; $i <= 6; $i++) {echo $i . " ";}
+    echo "<br>";
+    echo "</td>";
+    echo "</tr>";
+
+
+    echo "<br>";
+    echo "</table>";
+    ?>
+</body>
+</html>
