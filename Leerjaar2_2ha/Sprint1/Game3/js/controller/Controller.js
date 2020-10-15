1680dd5b2c6320cde9f5479ed5bab47fbb4da7fa
Justin Schouten <302807392@student.rocmondriaan.nl>
2020-10-15 18:48:29 +0200

    this notatie

--- Leerjaar2_2ha/Sprint1/Game3/js/controller/Controller.js
+++ Leerjaar2_2ha/Sprint1/Game3/js/controller/Controller.js
@@ -4,18 +4,66 @@ import {VierView} from "../view/VierView.js";
 export class Controller {
 
     constructor() {
-        this.vierOpEenRij = new VierModel();
-        this.vierOpEenRijView = new VierView();
+        this.view = new VierView(this);
+        this.model = new VierModel(this.view);
 
-        this.vierOpEenRijView.bindStartGame(this.handleStartGame);
-        this.vierOpEenRijView.bindRestartGame(this.handleStartGame);
-        this.vierOpEenRijView.showSpeelBord();
+        this.model.prepareTheGame(this.handleStartGame);
+        this.eventListener();
 
     }
 
     handleStartGame = (e) => {
         alert("De game word gestart");
-        this.vierOpEenRij.prepareTheGame();
-        this.vierOpEenRijView.showSpeelBord();
+        this.model.prepareTheGame();
+    };
+
+// Event Handlers
+    handleCellMouseOver = (e) => {
+        if (!this.model.gameIsLive) return;
+        this.model.cell = e.target;
+        const [rowIndex, colIndex] = this.model.getCellLocation(this.cell);
+
+        this.topCell = this.model.topCells[colIndex];
+        this.topCell.classList.add(this.yellowIsNext ? 'yellow' : 'red');
+
+    };
+
+    handleCellMouseOut = (e) => {
+        this.cell = e.target;
+        const [rowIndex, colIndex] = this.model.getCellLocation(this.cell);
+        this.model.clearColorFromTop(colIndex);
+    };
+
+    handleCellClick = (e) => {
+        if (!this.model.gameIsLive) return;
+        this.cell = e.target;
+        const [rowIndex, colIndex] = this.model.getCellLocation(this.cell);
+
+        this.openCell = this.model.getFirstOpenCellForColumn(colIndex);
+
+        if (!this.model.openCell) return;
+
+        this.openCell.classList.add(this.model.yellowIsNext ? 'yellow' : 'red');
+        this.model.checkStatusOfGame(this.openCell);
+
+        this.yellowIsNext = !this.yellowIsNext;
+        this.model.clearColorFromTop(colIndex);
+
+        if (this.model.gameIsLive) {
+            this.topCell = this.model.topCells[colIndex];
+            this.topCell.classList.add(this.yellowIsNext ? 'yellow' : 'red');
+        }
+    };
+
+
+// Adding Event Listeners
+    eventListener() {
+        for (let row of this.model.rows) {
+            for (this.cell of row) {
+                this.cell.addEventListener('mouseover', this.handleCellMouseOver());
+                this.cell.addEventListener('mouseout', this.handleCellMouseOut());
+                this.cell.addEventListener('click', this.handleCellClick);
+            }
+        }
     }
 }
--- Leerjaar2_2ha/Sprint1/Game3/js/view/VierView.js
+++ Leerjaar2_2ha/Sprint1/Game3/js/view/VierView.js
@@ -1,3 +1,22 @@
 export class VierView {
 
+    constructor(view) {
+        this.view = view;
+    }
+
+    // restbutton
+    resetButton() {
+        this.resetButton.addEventListener('click', () => {
+            for (const row of this.view.rows) {
+                for (const cell of row) {
+                    cell.classList.remove('red');
+                    cell.classList.remove('yellow');
+                    cell.classList.remove('win');
+                }
+            }
+            this.view.gameIsLive = true;
+            this.view.yellowIsNext = true;
+            this.view.statusSpan.textContent = ' ';
+        });
+    }
 }
